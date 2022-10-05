91e2836f8a59f45ffdaa31b3a83b08202029bb0d
Raymon S <raymon@raymonschouwenaar.nl>
2022-10-05 09:56:34 +0200

    Add Vue slide #5

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -1,27 +1,90 @@
 <template>
-  <a-scene embedded id="scene" background="color: rgb(255, 231, 66);" vr-mode-ui="enabled: false;" renderer="colorManagement: true;" screenshot>
-    <a-entity id="rig" position="25 10 0">
-      <a-entity id="camera" camera look-controls wasd-controls></a-entity>
-    </a-entity>
-    <a-assets>
-      <!--<a-asset-item id="glove" src="../src/assets/obj/boxingGlove/scene.gltf"></a-asset-item>-->
-      <a-asset-item id="frontendObj" src="../src/assets/obj/frontend developer.obj"></a-asset-item>
-      <a-asset-item id="sharevalueObj" src="../src/assets/obj/sv3.obj"></a-asset-item>
-      <a-asset-item id="reactRocketObj" src="../src/assets/obj/react-rocket.obj"></a-asset-item>
-      <a-asset-item id="rocketObj" src="../src/assets/obj/10475_Rocket_Ship_v1_L3.obj"></a-asset-item>
-      <a-asset-item id="angularObj" src="../src/assets/obj/angular.obj"></a-asset-item>
-      <img id="react-img" src="../assets/img/react-img.png">
-      <img id="angular-img" src="../assets/img/angular.png">
-    </a-assets>
-    <!--<a-gltf-model scale="0.01 0.01 0.01" position="25 10 -7" rotation="0 180 0" src="#glove"></a-gltf-model>-->
-    <a-entity id="front-end" :position="frontEnd.x + ' ' + frontEnd.y + ' ' + frontEnd.z" rotation="90 0 0" material="color: #000000;" obj-model="obj: #frontendObj;"></a-entity>
-    <a-entity id="sharevalue" :position="sharevalue.x + ' ' + sharevalue.y + ' ' + sharevalue.z" rotation="90 0 0" material="color: #000000;" obj-model="obj: #sharevalueObj;"></a-entity>
-    <a-entity id="react-rocket" :position="reactRocket.x + ' ' + reactRocket.y + ' ' + reactRocket.z" rotation="90 0 0" material="color: #fff;" obj-model="obj: #reactRocketObj;"></a-entity>
-    <a-entity id="rocket" scale="0.002 0.002 0.002" :position="rocket.x + ' ' + rocket.y + ' ' + rocket.z" material="color: #f00;" rotation="-45 90 -90" obj-model="obj: #rocketObj;"></a-entity>
-    <a-image id="react" :position="react.x + ' ' + react.y + ' ' + react.z" rotation="0 0 0" src="#react-img"></a-image>
-    <a-entity id="angularText" :position="angularText.x + ' ' + angularText.y + ' ' + angularText.z" rotation="90 0 0" material="color: #ffffff;" obj-model="obj: #angularObj;"></a-entity>
-    <a-image id="angular" :position="angular.x + ' ' + angular.y + ' ' + angular.z" rotation="0 0 0" src="#angular-img"></a-image>
-  </a-scene>
+	<a-scene
+		embedded
+		id="scene"
+		background="color: rgb(255, 231, 66);"
+		vr-mode-ui="enabled: false;"
+		renderer="colorManagement: true;"
+		screenshot
+	>
+		<a-entity id="rig" position="25 10 0">
+			<a-entity id="camera" camera look-controls="false" wasd-controls="false"></a-entity>
+		</a-entity>
+		<a-assets>
+			<!--<a-asset-item id="glove" src="../src/assets/obj/boxingGlove/scene.gltf"></a-asset-item>-->
+			<a-asset-item id="frontendObj" src="../src/assets/obj/frontend developer.obj"></a-asset-item>
+			<a-asset-item id="sharevalueObj" src="../src/assets/obj/sv3.obj"></a-asset-item>
+			<a-asset-item id="reactRocketObj" src="../src/assets/obj/react-rocket.obj"></a-asset-item>
+			<a-asset-item id="rocketObj" src="../src/assets/obj/10475_Rocket_Ship_v1_L3.obj"></a-asset-item>
+			<a-asset-item id="angularObj" src="../src/assets/obj/angular.obj"></a-asset-item>
+			<a-asset-item id="vueObj" src="../src/assets/obj/vue.obj"></a-asset-item>
+			<img id="react-img" src="../assets/img/react-img.png" />
+			<img id="angular-img" src="../assets/img/angular.png" />
+			<img id="vue-img" src="../assets/img/vue.png" />
+		</a-assets>
+		<!--<a-gltf-model scale="0.01 0.01 0.01" position="25 10 -7" rotation="0 180 0" src="#glove"></a-gltf-model>-->
+		<a-entity
+			id="front-end"
+			:position="frontEnd.x + ' ' + frontEnd.y + ' ' + frontEnd.z"
+			rotation="90 0 0"
+			material="color: #000000;"
+			obj-model="obj: #frontendObj;"
+		></a-entity>
+		<a-entity
+			id="sharevalue"
+			:position="sharevalue.x + ' ' + sharevalue.y + ' ' + sharevalue.z"
+			rotation="90 0 0"
+			material="color: #000000;"
+			obj-model="obj: #sharevalueObj;"
+		></a-entity>
+		<a-entity
+			id="react-rocket"
+			:position="reactRocket.x + ' ' + reactRocket.y + ' ' + reactRocket.z"
+			rotation="90 0 0"
+			material="color: #fff;"
+			obj-model="obj: #reactRocketObj;"
+		></a-entity>
+		<a-entity
+			id="rocket"
+			scale="0.002 0.002 0.002"
+			:position="rocket.x + ' ' + rocket.y + ' ' + rocket.z"
+			material="color: #f00;"
+			rotation="-45 90 -90"
+			obj-model="obj: #rocketObj;"
+		></a-entity>
+		<a-image
+			id="react"
+			:position="react.x + ' ' + react.y + ' ' + react.z"
+			rotation="0 0 0"
+			src="#react-img"
+		></a-image>
+		<a-entity
+			id="angularText"
+			:position="angularText.x + ' ' + angularText.y + ' ' + angularText.z"
+			rotation="90 0 0"
+			material="color: #ffffff;"
+			obj-model="obj: #angularObj;"
+		></a-entity>
+		<a-image
+			id="angular"
+			:position="angular.x + ' ' + angular.y + ' ' + angular.z"
+			rotation="0 0 0"
+			src="#angular-img"
+		></a-image>
+		<a-entity
+			id="vueText"
+			:position="vueText.x + ' ' + vueText.y + ' ' + vueText.z"
+			rotation="90 0 0"
+			material="color: #ffffff;"
+			obj-model="obj: #reactRocketObj;"
+		></a-entity>
+		<a-image
+			id="vueImage"
+			:position="vueImage.x + ' ' + vueImage.y + ' ' + vueImage.z"
+			rotation="0 0 0"
+			src="#vue-img"
+		></a-image>
+	</a-scene>
 </template>
 
 <script lang="ts">
@@ -32,7 +95,7 @@ export default {
     return {
       scrollPage: false,
       scrollFase: 1,
-      animationScrollSpeed: 0.25,
+      animationScrollSpeed: 0.5,
       frontEnd: {
         x: 20.5,
         y: 10.7,
@@ -68,6 +131,16 @@ export default {
         y: 19.5,
         z: -2,
       },
+      vueText: {
+        x: 22.25,
+        y: 19.5,
+        z: -4.6,
+      },
+      vueImage: {
+        x: 24.95,
+        y: 19.5,
+        z: -2,
+      },
     }
   },
   methods: {},
@@ -84,13 +157,23 @@ export default {
     let angularR = 221;
     let angularG = 27;
     let angularB = 22;
+    let vueR = 65;
+    let vueG = 184;
+    let vueB = 131;
     let reactPassed = false;
     let angularPassed = false;
+    let vuePassed = false;
     document.addEventListener('wheel', (e) => {
       if (e.type != 'wheel') { return; }
       let delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;
       delta = delta * -1;
-      
+
+
+      /**
+       *
+       * PHASE 1: REACT
+       *
+       */
       if (this.scrollFase === 1) {
         /** Scroll fase 1: FRONTEND => REACT */
         if (delta === -1 || (delta === 1 && this.frontEnd.y <= (10.7 - this.animationScrollSpeed))) {
@@ -141,6 +224,12 @@ export default {
         if (this.rocket.y > 16 && reactPassed) {
           this.scrollFase = 2;
         }
+
+        /**
+       *
+       * PHASE 2: ANGULAR
+       *
+       */
       } else if (this.scrollFase === 2) {
         /** Scroll fase 2: REACT => ANGULAR */
         if (this.angularText.y > 11.4 || delta === 1) {
@@ -173,7 +262,45 @@ export default {
         if (angularPassed && this.angular.y <= 9.5) {
           this.scrollFase = 3;
         }
+
+        /**
+       *
+       * PHASE 3: VUE
+       *
+       */
       } else if (this.scrollFase === 3) {
+        /** Scroll fase 3: ANGULAR => VUE */
+        if (this.vueText.y > 11.4 || delta === 1) {
+          this.vueText.y += (this.animationScrollSpeed * delta);
+        }
+        if (this.vueText.y < 15 || delta === 1) {
+          this.angularText.y += (this.animationScrollSpeed * delta);
+          this.angular.y += (this.animationScrollSpeed * delta);
+        }
+        if (this.vueImage.y > 9.5 || delta === 1) {
+          this.vueImage.y += (this.animationScrollSpeed * delta);
+        }
+        if (!vuePassed && delta === -1) {
+          if (colorR !== vueR) {
+            colorR += 1;
+          }
+          if (colorG !== vueG) {
+            colorG -= 1;
+          }
+          if (colorB !== vueB) {
+            colorB -= 1;
+          }
+          if (colorR === vueR
+            && colorG === vueG
+            && colorB === vueB) {
+              vuePassed = true;
+            }
+            document.getElementById('scene').setAttribute('background', `color: rgb(${colorR}, ${colorG}, ${colorB});`);
+        }
+        if (vuePassed && this.vueImage.y <= 9.5) {
+          this.scrollFase = 4;
+        }
+      } else if (this.scrollFase === 4) {
         this.$emit('animationDone');
       }
       if (delta === 1 && window.scrollY === 0) {
@@ -183,7 +310,7 @@ export default {
   },
   computed: {
     box_style() {
-      return { 
+      return {
         transform: `
           perspective(${this.perspective}px)
           rotateX(${this.rotateX}deg)
@@ -197,9 +324,8 @@ export default {
 </script>
 
 <style lang="scss">
-
 #scene {
-  width: 100vw;
-  height: 100vh;
+	width: 100vw;
+	height: 100vh;
 }
 </style>
